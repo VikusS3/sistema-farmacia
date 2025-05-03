f23f0a90bc42d6b3dfc28ce442cfe9664ee57ac7
SaulDV <felix21soc@gmail.com>
2025-05-03 14:49:25 -0500

    implementacion de useQuery

---
+++ frontend/app/lib/react-query.ts
@@ -0,0 +1,11 @@
+// src/app/lib/react-query.ts
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutos de cache
+      retry: 1, // Reintentar una vez si falla
+    },
+  },
+});
