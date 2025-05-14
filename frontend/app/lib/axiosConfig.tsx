3b17a1db8ea87dbaca6d37baa75800e64f121124
SaulDV <felix21soc@gmail.com>
2025-05-14 10:07:34 -0500

    Impelementacion de interceptores en el archivo de configruacion de axios
    esto para detectar si el usario hace un logout y obtener el token mas reciente

--- frontend/app/lib/axiosConfig.tsx
+++ frontend/app/lib/axiosConfig.tsx
@@ -7,4 +7,18 @@ const api = axios.create({
   },
 });
 
+// Interceptor para agregar el token en cada solicitud
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default api;
