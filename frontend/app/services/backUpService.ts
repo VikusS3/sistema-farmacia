0c6dce0ef97edbcdb04be8b83e856c7af1692221
SaulDV <felix21soc@gmail.com>
2025-05-28 10:46:27 -0500

    Update backUpService.ts

--- frontend/app/services/backUpService.ts
+++ frontend/app/services/backUpService.ts
@@ -1,11 +1,12 @@
 import api from "../lib/axiosConfig";
+import Swal from "sweetalert2";
 
 export const createBackup = async () => {
-    try {
-        await api.post("/backup");
-        alert("Backup creado correctamente");
-    } catch (error) {
-        console.error("Error al crear el backup:", error);
-        alert("Error al crear el backup");
-    }
+  try {
+    await api.post("/backup");
+    Swal.fire("Éxito", "Backup creado correctamente", "success");
+  } catch (error) {
+    console.error("Error al crear el backup:", error);
+    Swal.fire("Error", "Error al crear el backup", "error");
+  }
 };
