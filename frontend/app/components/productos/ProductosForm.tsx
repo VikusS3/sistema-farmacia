033c3d13259328e164f3d581eabf5cbad35efa22
SaulDV <felix21soc@gmail.com>
2025-08-25 08:30:40 -0500

    Update ProductosForm.tsx

--- frontend/app/components/productos/ProductosForm.tsx
+++ frontend/app/components/productos/ProductosForm.tsx
@@ -257,7 +257,7 @@ export default function ProductosForm({
           className="bg-green-600 text-white py-2 px-4 rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 disabled:opacity-50"
           disabled={loading}
         >
-          {editingProductoId ? "Editar" : "Agregar"}
+          {editingProductoId ? "Editar" : "Registrar"}
         </button>
       </div>
     </form>
