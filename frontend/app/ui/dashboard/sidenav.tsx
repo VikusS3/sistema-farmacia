1b314d41f6904c870a2454db3a97b13f3aee1269
SaulDV <felix21soc@gmail.com>
2025-08-25 09:40:58 -0500

    Update sidenav.tsx

--- frontend/app/ui/dashboard/sidenav.tsx
+++ frontend/app/ui/dashboard/sidenav.tsx
@@ -2,10 +2,11 @@
 import NavLinks from "./nav-links";
 import Image from "next/image";
 import { useAuth } from "@/app/hooks/auth/useAuth";
+import { LogOutIcon } from "lucide-react";
 export default function SideNav() {
   const { handleLogout } = useAuth();
   return (
-    <nav className="bg-background-200 w-64 h-screen border-r border-background-300 overflow-y-auto">
+    <nav className="flex flex-col bg-background-200 w-64 h-screen border-r border-background-300 overflow-y-auto">
       <div className="flex items-center justify-center h-16 shadow-lg border-b border-background-300">
         <Image
           src="/logofarmacia.webp"
@@ -23,10 +24,10 @@ export default function SideNav() {
       </div>
       <div className="flex items-center justify-center h-16 shadow-lg border-b border-background-300">
         <button
-          className="bg-red-500 text-white px-4 py-2 rounded"
+          className="bg-red-500 text-white px-4 py-2 rounded flex items-center gap-2"
           onClick={handleLogout}
         >
-          Salir
+          <LogOutIcon className="w-4 h-4" /> Salir
         </button>
       </div>
     </nav>
